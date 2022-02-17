847439716ad32547d878527bf5e60fe24badad95
Abdullahi Yusuf <abdul.benyusuf@gmail.com>
2022-02-17 19:45:46 +0100

    Added Factorial and Fibonacci

--- src/strings/1.LengthOfLastWord/csharp.cs
+++ src/strings/1.LengthOfLastWord/csharp.cs
@@ -1,4 +1,11 @@
 /// https://leetcode.com/problems/length-of-last-word/
+/*
+
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+*/
 public class Solution {
     public int LengthOfLastWord(string s) {
         int n = s.Length, i = 0, count = 0;
