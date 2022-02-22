a54068fa4e86fb967c1b6c624d0abec1ca508a37
Abdullahi Yusuf <abdul.benyusuf@gmail.com>
2022-02-22 16:54:43 +0100

    Added number of rotations

---
+++ src/searching/2.BinarySearch/number-of-rotations/cplusplus.cpp
@@ -0,0 +1,28 @@
+/// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+#include <vector>
+class Solution {
+public:
+    int findMin(vector<int>& A) {
+        int s=0;
+        int n = A.size();
+        int e = n-1;
+        
+        while(s<=e){
+            int mid = (s+e)/2;
+            int next = (mid+1)%n;
+            int prev = (mid-1+n)%n;
+            
+            if(A[mid]<=A[prev] && A[mid]<=A[next]){
+               /// how many times a sorted array is rotated
+               ///return mid;
+                
+                return mid;
+            }else if(A[mid]<=A[e]){
+                e = mid - 1;
+            }else if(A[mid] >= A[s]){
+                s = mid + 1;
+            }
+        }
+        return -1;  /// compilation purpose
+    }
+};
