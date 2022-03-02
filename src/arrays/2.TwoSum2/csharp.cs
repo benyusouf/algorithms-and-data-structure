5d65445bc41fcd55d3f1773add68a27b02d9e164
Abdullahi Yusuf <abdul.benyusuf@gmail.com>
2022-03-02 22:15:40 +0100

    Added two sum 2

---
+++ src/arrays/2.TwoSum2/csharp.cs
@@ -0,0 +1,27 @@
+/// <summary>
+/// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+/// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+/// Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+/// The tests are generated such that there is exactly one solution. You may not use the same element twice.
+/// Your solution must use only constant extra space.
+/// </summary>
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int s = 0, e = numbers.Length -1;
+        int[] ans = new int[2];
+        
+        while(s < e){
+            if((numbers[s] + numbers[e]) == target){
+                ans[0] = s+1;
+                ans[1] = e+1;
+                return ans;
+            }
+            else if((numbers[s] + numbers[e]) > target)
+                e--;
+            else 
+                s++;
+        }
+        
+        return ans;
+    }
+}
