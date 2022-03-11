155c4cfb6b29491dbc713f6b868a3791563b3f29
Abdullahi Yusuf <abdul.benyusuf@gmail.com>
2022-03-11 12:14:05 +0100

    Added MaxSubArray

---
+++ src/arrays/3.MaxSubArray/cplusplus.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int curSum = 0;
+        for(int i=0;i<nums.size();i++){
+            curSum += nums[i];
+            maxSum = max(maxSum , curSum);
+            if(curSum <0){
+                curSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
